ae63034613fe620ed683f4fe23155646d110402b
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-04-12 12:19:32 +0530

    Added some photos

--- src/Components/NavbarVidio/NavbarVidio.jsx
+++ src/Components/NavbarVidio/NavbarVidio.jsx
@@ -39,13 +39,13 @@ export default class NavbarVidio extends Component {
   };
 
   slides = [
-    { key: uuidv4(), content: <img src="https://picsum.photos/800/801/?random" alt="1" /> },
-    { key: uuidv4(), content: <img src="https://picsum.photos/800/802/?random" alt="2" /> },
-    { key: uuidv4(), content: <img src="https://picsum.photos/600/803/?random" alt="3" /> },
+    { key: uuidv4(), content: <img src="/clg6.jpeg" alt="1" /> },
+    { key: uuidv4(), content: <img src="/clg1.jpeg" alt="2" /> },
+    { key: uuidv4(), content: <img src="/clg4.jpeg" alt="3" /> },
     { key: uuidv4(), content: <img src="https://picsum.photos/800/500/?random" alt="4" /> },
-    { key: uuidv4(), content: <img src="https://picsum.photos/800/804/?random" alt="5" /> },
-    { key: uuidv4(), content: <img src="https://picsum.photos/500/800/?random" alt="6" /> },
-    { key: uuidv4(), content: <img src="https://picsum.photos/800/600/?random" alt="7" /> },
+    { key: uuidv4(), content: <img src="/clg3.jpeg" alt="5" /> },
+    { key: uuidv4(), content: <img src="/clg2.jpeg" alt="6" /> },
+    { key: uuidv4(), content: <img src="/clg5.jpeg" alt="7" /> },
     { key: uuidv4(), content: <img src="https://picsum.photos/805/800/?random" alt="8" /> }
   ].map((slide, index) => {
     return { ...slide };
